7498787149574599c67a628c343fea7987bcb5ba
Abu Talha <talha007abu@gmail.com>
2025-08-03 23:12:34 +0600

    day 03

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/final/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/final/script.js
@@ -42,19 +42,19 @@ if (hasDriversLicense) console.log('I can drive :D');
 // Function Declarations vs. Expressions
 
 // Function declaration
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-}
-const age1 = calcAge1(1991);
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991);
 
-// Function expression
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
-}
-const age2 = calcAge2(1991);
-console.log(age1, age2);
+// // Function expression
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// }
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
 
-
+// done 02/08/2025
 /////////////////////////////////////
 // Arrow functions
 
